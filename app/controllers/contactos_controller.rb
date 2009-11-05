45dd93a6b18a45a30990b32cbe7147c46c41342f
jhass05 <jhass05@hotmail.com>
2009-11-05 08:35:10 -0300

    completado contactos

--- app/controllers/contactos_controller.rb
+++ app/controllers/contactos_controller.rb
@@ -1,15 +1,7 @@
 class ContactosController < ApplicationController
   
   layout "main"
-  
-  def new
-     @contacto = Contacto.new
-  end
-   
-  def create 
-    redirect_to :action => :new
-  end
-  
+    
   def index
     
   end
